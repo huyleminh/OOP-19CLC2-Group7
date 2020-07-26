6edc2f936ed7c6578a9ff15d0af99c370a9fff51
huyleminh <leminhhuy.hcmus@gmail.com>
2020-07-26 15:34:32 +0700

    Update Menu.cpp

--- OOP-19CLC2-Group7/Bus-Reservation-System-C++-Project/Menu.cpp
+++ OOP-19CLC2-Group7/Bus-Reservation-System-C++-Project/Menu.cpp
@@ -31,15 +31,16 @@ OPTION:
     case 5:
         exit(0);
     }
-
 }
 
 //If login successfully with Admin, render this menu
-void Menu::renderAdminMenu() {
+void Menu::renderAdminMenu(User& user) {
     cout << "WELCOME ADMIN\n";
-    cout << "Change password.\n";
-    cout << "Log out.\n";
-    cout << "Exit.\n";
+    cout << "1. Search bus no.\n";
+    cout << "2. Search departure and destination.\n";
+    cout << "3. Change password.\n";
+    cout << "4. Register.\n";
+    cout << "5. Exit app.\n";
 
     int option;
 OPTION:
@@ -47,19 +48,31 @@ OPTION:
     cin >> option;
 
     //Check valid option from 1 to 5.
-    // if (option < 1 || option > 5)
+    if (option < 1 || option > 5)
         goto OPTION;
 
     switch (option) {
+    case 1:
+        //Search bus no
+    case 2: 
+        //Search departure and destination
+    case 3:
+
+    case 4: 
+
+    case 5:
+        exit(0);
     }
 }
 
 //If login successfully with Diver, render this menu
-void Menu::renderDriverMenu() {
+void Menu::renderDriverMenu(User& user) {
     cout << "WELCOME DRIVER\n";
-    cout << "Change password.\n";
-    cout << "Log out.\n";
-    cout << "Exit.\n";
+    cout << "1. Search bus no.\n";
+    cout << "2. Search departure and destination.\n";
+    cout << "3. Change password.\n";
+    cout << "4. Register.\n";
+    cout << "5. Exit app.\n";
 
     int option;
 OPTION:
@@ -67,29 +80,51 @@ OPTION:
     cin >> option;
 
     //Check valid option from 1 to 5.
-    // if (option < 1 || option > 5)
+    if (option < 1 || option > 5)
         goto OPTION;
 
     switch (option) {
+    case 1:
+        //Search bus no
+    case 2: 
+        //Search departure and destination
+    case 3:
+
+    case 4: 
+
+    case 5:
+        exit(0);
     }
 }
 
 //If login successfully with Passenger, render this menu
-void Menu::renderPassengerMenu() {
+void Menu::renderPassengerMenu(User& user) {
     cout << "WELCOME PASSENGER\n";
-    cout << "Change password.\n";
-    cout << "Log out.\n";
-    cout << "Exit.\n";
+    cout << "1. Search bus no.\n";
+    cout << "2. Search departure and destination.\n";
+    cout << "3. Change password.\n";
+    cout << "4. Register.\n";
+    cout << "5. Exit app.\n";
 
     int option;
 OPTION:
     cout << "Enter you option: ";
     cin >> option;
 
-    //Check valid option 
-    // if (option < 1 || option > 5)
+    //Check valid option from 1 to 5.
+    if (option < 1 || option > 5)
         goto OPTION;
 
     switch (option) {
+    case 1:
+        //Search bus no
+    case 2: 
+        //Search departure and destination
+    case 3:
+
+    case 4: 
+
+    case 5:
+        exit(0);
     }
 }
