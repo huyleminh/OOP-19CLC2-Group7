23cdce8a2c0959dbe1cf8ce59db8c6598ab459c2
huyleminh <leminhhuy.hcmus@gmail.com>
2020-07-26 10:43:48 +0700

    Update Menu.cpp

--- OOP-19CLC2-Group7/Bus-Reservation-System-C++-Project/Menu.cpp
+++ OOP-19CLC2-Group7/Bus-Reservation-System-C++-Project/Menu.cpp
@@ -1,1 +1,95 @@
 #include "Menu.h"
+#include "User.h"
+
+//Render main menu after start app
+void Menu::renderMainMenu() {
+    cout << "MENU\n";
+    cout << "1. Search bus no.\n";
+    cout << "2. Search departure and destination.\n";
+    cout << "3. Log in.\n";
+    cout << "4. Register.\n";
+    cout << "5. Exit app.\n";
+
+    int option;
+OPTION:
+    cout << "Enter you option: ";
+    cin >> option;
+
+    //Check valid option from 1 to 5.
+    if (option < 1 || option > 5)
+        goto OPTION;
+
+    switch (option) {
+    case 1:
+        //Search bus no
+    case 2: 
+        //Search departure and destination
+    case 3:
+
+    case 4: 
+
+    case 5:
+        exit(0);
+    }
+
+}
+
+//If login successfully with Admin, render this menu
+void Menu::renderAdminMenu() {
+    cout << "WELCOME ADMIN\n";
+    cout << "Change password.\n";
+    cout << "Log out.\n";
+    cout << "Exit.\n";
+
+    int option;
+OPTION:
+    cout << "Enter you option: ";
+    cin >> option;
+
+    //Check valid option from 1 to 5.
+    // if (option < 1 || option > 5)
+        goto OPTION;
+
+    switch (option) {
+    }
+}
+
+//If login successfully with Diver, render this menu
+void Menu::renderDriverMenu() {
+    cout << "WELCOME DRIVER\n";
+    cout << "Change password.\n";
+    cout << "Log out.\n";
+    cout << "Exit.\n";
+
+    int option;
+OPTION:
+    cout << "Enter you option: ";
+    cin >> option;
+
+    //Check valid option from 1 to 5.
+    // if (option < 1 || option > 5)
+        goto OPTION;
+
+    switch (option) {
+    }
+}
+
+//If login successfully with Passenger, render this menu
+void Menu::renderPassengerMenu() {
+    cout << "WELCOME PASSENGER\n";
+    cout << "Change password.\n";
+    cout << "Log out.\n";
+    cout << "Exit.\n";
+
+    int option;
+OPTION:
+    cout << "Enter you option: ";
+    cin >> option;
+
+    //Check valid option 
+    // if (option < 1 || option > 5)
+        goto OPTION;
+
+    switch (option) {
+    }
+}
