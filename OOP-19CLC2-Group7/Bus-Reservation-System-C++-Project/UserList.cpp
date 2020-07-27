9d3f1865d1ae6b91a5872117490da66000b02c05
huyleminh <leminhhuy.hcmus@gmail.com>
2020-07-27 23:59:06 +0700

    Update UserList.cpp

--- OOP-19CLC2-Group7/Bus-Reservation-System-C++-Project/UserList.cpp
+++ OOP-19CLC2-Group7/Bus-Reservation-System-C++-Project/UserList.cpp
@@ -1,21 +1,21 @@
 #include "UserList.h"
 
-//Operators
-ifstream& operator >>(ifstream& ifs, User& user) {
-    return ifs;
-}
-
-ofstream& operator <<(ofstream& ofs, const User& user) {
-    return ofs;
-}
-
-ostream& operator <<(ostream& os, const User& user) {
-    return os;
-}
-
-//Destructor
-UserList::~UserList() {
-    int len = this->_list.size();
-    for (int i =0; i < len; i++)
-        this->_list.pop_back();
-}
+////Operators
+//ifstream& operator >>(ifstream& ifs, User& user) {
+//    return ifs;
+//}
+//
+//ofstream& operator <<(ofstream& ofs, const User& user) {
+//    return ofs;
+//}
+//
+//ostream& operator <<(ostream& os, const User& user) {
+//    return os;
+//}
+//
+////Destructor
+//UserList::~UserList() {
+//    int len = this->_list.size();
+//    for (int i =0; i < len; i++)
+//        this->_list.pop_back();
+//}
