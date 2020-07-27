7cdb290114afa563ea62ea781f423ab550218fce
huyleminh <leminhhuy.hcmus@gmail.com>
2020-07-27 23:58:59 +0700

    Update main.cpp

--- OOP-19CLC2-Group7/Bus-Reservation-System-C++-Project/main.cpp
+++ OOP-19CLC2-Group7/Bus-Reservation-System-C++-Project/main.cpp
@@ -1,20 +1,11 @@
 #include "Header.h"
 #include "Menu.h"
+#include "User.h"
 #include "BusList.h"
 
 int main() {
-	BusList a;
-	ifstream f1("../Data\\Buses.txt");
-	if (!f1.is_open())
-	{
-		cout << "Can not open file";
-	}
-	else
-	{
-		f1 >> a;
-		a.PrintList();
-		a.Show();
-		f1.close();
-	}
+	User user;
+	cin >> user;
+	cout << user;
 	return 0;
 }
