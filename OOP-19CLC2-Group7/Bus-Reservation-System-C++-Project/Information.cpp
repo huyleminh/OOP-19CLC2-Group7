59c979ce9e8368088efd5b287d375af277ac0c01
thuan-NQT <64117378+thuan-NQT@users.noreply.github.com>
2020-08-06 10:25:06 +0700

    role 1

--- OOP-19CLC2-Group7/Bus-Reservation-System-C++-Project/Information.cpp
+++ OOP-19CLC2-Group7/Bus-Reservation-System-C++-Project/Information.cpp
@@ -1,3 +1,88 @@
 #include "Information.h"
 
+Information::Information()
+{
+	this->name = "No name";
+	this->sex = "male";
+	this->birthday = "01/01/1990";
+	this->CMNN = "xxxxxxxxx";
+	this->phone = "0000000000";
+}
 
+Information::Information(const Information& info)
+{
+	*this = info;
+}
+
+Information& Information::operator=(const Information& info)
+{
+	*this = info;
+	return *this;
+}
+
+ifstream& operator>>(ifstream& ifs, Information& info)
+{
+	string temp="";
+
+	getline(ifs, temp, '\n');
+
+	getline(ifs, temp, '\n');
+	info.name = temp;
+
+	getline(ifs, info.sex, '\n');
+	info.sex = temp;
+
+	getline(ifs, temp, '\n');
+	info.birthday = temp;
+
+	getline(ifs, temp, '\n');
+	info.CMNN = temp;
+
+	getline(ifs, temp, '\n');
+	info.phone = temp;
+
+	return ifs;
+}
+
+ofstream& operator<<(ofstream& ofs, const Information& info)
+{
+	ofs << info.name << endl;
+	ofs << info.sex << endl;
+	ofs << info.birthday << endl;
+	ofs << info.CMNN << endl;
+	ofs << info.phone << endl;
+	ofs << endl;
+
+	return ofs;
+}
+
+istream& operator>>(istream& is, Information& info)
+{
+	cout << "enter name: ";
+	getline(is, info.name, '\n');
+
+	cout << "enter sex (MALE/FEMALE): ";
+	getline(is, info.sex, '\n');
+
+	cout << "enter birthday(dd/mm/yy): ";
+	getline(is, info.birthday, '\n');
+
+	cout << "enter CMNN: ";
+	getline(is, info.CMNN, '\n');
+
+	cout << "enter phone: ";
+	getline(is, info.phone, '\n');
+
+	return is;
+}
+
+ostream& operator<<(ostream& os, const Information& info)
+{
+	cout << "Name: " << info.name << endl;
+	cout << "Sex: " << info.sex << endl;
+	cout << "Birthday: " << info.birthday << endl;
+	cout << "CMNN: " << info.CMNN << endl;
+	cout << "Phone: " << info.phone << endl;
+
+	return os;
+}
--- OOP-19CLC2-Group7/Bus-Reservation-System-C++-Project/Information.h
+++ OOP-19CLC2-Group7/Bus-Reservation-System-C++-Project/Information.h
@@ -5,6 +5,21 @@
 
 class Information
 {
+protected:
+    string name;
+    string sex; 
+    string birthday;
+    string CMNN;
+    string phone;
+
+public:
+    Information();
+    Information(const Information& info);
+    Information& operator=(const Information& info);
+    friend ifstream& operator >>(ifstream& ifs, Information&info);
+    friend ofstream& operator <<(ofstream& ofs, const Information& info);
+    friend istream& operator >>(istream& is, Information& info);
+    friend ostream& operator <<(ostream& os, const Information& info);
 
 };
 
