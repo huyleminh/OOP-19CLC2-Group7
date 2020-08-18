c3662f7e814b80642ae82a5595a6538d7faf64ac
Akrlrd3306 <63332697+Akrlrd3306@users.noreply.github.com>
2020-08-18 22:26:59 +0700

    Not finish edit Information
    Add Driver and fix Passenger txt
    
    EditInfomation: need to add codes to rewrite the edit Information to text files

--- OOP-19CLC2-Group7/Bus-Reservation-System-C++-Project/Passenger.cpp
+++ OOP-19CLC2-Group7/Bus-Reservation-System-C++-Project/Passenger.cpp
@@ -1,4 +1,4 @@
-#include "Passenger.h"
+﻿#include "Passenger.h"
 
 //Constructor
 Passenger::Passenger() : Information() {
@@ -17,6 +17,122 @@ Passenger& Passenger::operator =(const Passenger& p) {
 	this->_type = p._type;
 	return *this;
 }
+
+void Passenger::changeInformation(User& user)
+{
+	vector<Information> passenger;// tính đọc từ file rồi lưu vào vector
+	vector<string> role;
+	int pos = -1; // position
+	ifstream fin;
+	int option;
+	string newName;
+	string newGender;
+	string newDob;
+	int stop = 1;
+	fin.open(".\\Data\\Passenger.txt");
+
+	if (!fin.is_open())
+	{
+		cout << "Can not open file";
+	}
+
+	else
+	{
+		Information infotemp;
+		string roletemp = "";
+		while (!fin.eof())
+		{
+			fin >> infotemp;
+			passenger.push_back(infotemp);
+
+		}
+
+		for (int i = 0; i < passenger.size(); i++)
+		{
+
+			if (this->_user == user)
+			{
+				pos = i;
+			}
+		}
+
+		do {
+
+			cout << "1.Name" << endl;
+			cout << "2.Sex" << endl;
+			cout << "3.Date of birth" << endl;
+
+			cout << "Choose the option you want to edit(1-3):";
+
+			do {
+				cin >> option;
+
+				if (cin.fail())
+				{
+					cin.clear();
+					cin.ignore(32767, '\n');
+				}
+
+				if (option < 1 || option > 3)
+				{
+					cout << "Invaild choice!!! Choose again: ";
+				}
+			} while (option < 1 || option > 3);
+
+			if (option == 1)
+			{
+				cout << "Enter new name: ";
+				getline(cin, newName);
+				changeName(newName);
+			}
+
+			if (option == 2)
+			{
+				cout << "Input your new gender: ";
+				getline(cin, newGender);
+				changeSex(newGender);
+			}
+
+			if (option == 3)
+			{
+
+				cout << "Input example: 01/01/2001" << endl;
+				cout << "Enter your new Dob: " << endl;
+				getline(cin, newDob);
+				changeBirthday(newDob);
+			}
+
+			cout << "Do you want to continue edit: ";
+			cout << "1.Yes" << endl;
+			cout << "0.No" << endl;
+			cin >> stop;
+		} while (stop == 1);
+	}
+
+	fin.close();
+}
+
+bool Passenger::changeName(const string& name)
+{
+	if(Information::changeName(name) == 1)
+		return true;
+	return false;
+}
+
+void Passenger::changeSex(const string& sex)
+{
+	Information::changeSex(sex);
+}
+
+bool Passenger::changeBirthday(const string& dob)
+{
+	if (Information::changeBirthday(dob) == 1)
+	{
+		return true;
+	}
+	return false;
+}
+
 
 ifstream& operator >>(ifstream& in, Passenger& p) {
 	in >> (Information&)(p);
