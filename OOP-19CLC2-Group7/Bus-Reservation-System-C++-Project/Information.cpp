6e65a3bc7fa36e5faf459ef82517f5728e3ca11e
huyleminh <leminhhuy.hcmus@gmail.com>
2020-08-24 13:55:08 +0700

    update method

--- OOP-19CLC2-Group7/Bus-Reservation-System-C++-Project/Information.cpp
+++ OOP-19CLC2-Group7/Bus-Reservation-System-C++-Project/Information.cpp
@@ -5,6 +5,11 @@ Information::Information()
 	this->_name = "Null";
 	this->_sex = "Null";
 	this->_birthday = "dd/mm/yyyy";
+	this->_username = "Null";
+}
+
+void Information::updateUsername(const string& username) {
+	this->_username = username;
 }
 
 Information::Information(const Information& info)
@@ -12,40 +17,17 @@ Information::Information(const Information& info)
 	this->_name = info._name;
 	this->_sex = info._sex;
 	this->_birthday = info._birthday;
+	this->_username = info._username;
 }
 
+//Operator
 Information& Information::operator=(const Information& info)
 {
 	this->_name = info._name;
 	this->_sex = info._sex;
 	this->_birthday = info._birthday;
+	this->_username = info._username;
 	return *this;
-}
-
-//Change and check success or not 
-bool Information::changeName(const string& name)
-{
-	const string regEx = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM ";
-	for (int i = 0; i < name.length(); i++)
-		if (regEx.rfind(name[i] == string::npos))
-			return false;
-	
-	vector<string> tmpStr = Tokenizer::split(name, " ");
-	for (int i = 0; i < tmpStr.size(); i++)
-		tmpStr[i][0] = tolower(tmpStr[i][0]);
-
-	this->_name = Tokenizer::join(tmpStr, " ");
-	return true;
-}
-
-void Information::changeSex(const string& sex)
-{
-	this->_sex = sex;
-}
-
-bool Information::changeBirthday(const string& birthday)
-{
-	return true;
 }
 
 ifstream& operator>>(ifstream& ifs, Information& info)
@@ -65,6 +47,9 @@ ifstream& operator>>(ifstream& ifs, Information& info)
 	getline(ifs, temp, '\n');
 	info._birthday = temp;
 
+	getline(ifs, temp, '\n');
+	info._username = temp;
+
 	return ifs;
 }
 
@@ -74,20 +59,35 @@ ofstream& operator<<(ofstream& ofs, const Information& info)
 	ofs << info._name << endl;
 	ofs << info._sex << endl;
 	ofs << info._birthday << endl;
-
+	ofs << info._username << endl;
 	return ofs;
 }
 
+//Need to fix 
 istream& operator>>(istream& is, Information& info)
 {
 	cout << "Enter name: ";
 	getline(is, info._name, '\n');
 
-	cout << "Enter sex (MALE/FEMALE): ";
-	getline(is, info._sex, '\n');
+	cout << "1. Male.\n";
+	cout << "2. Female.\n";
+	cout << "3. Others.\n";
+	cout << "Choose gender: ";
 
+	int option = 0;
+	is >> option;
+
+	if (option < 1 || option > 3)
+		info._sex = "Others";
+	
+	info._sex = ((option == 1) ? "Male" : (option == 2) ? "Female" : "Others");
+
+	is.ignore(1);
 	cout << "Enter birthday follow this format dd/mm/yyyy: ";
 	getline(is, info._birthday, '\n');
+
+	cout << "If any informations above is wrong, you can change it.\n";
+	Sleep(1000);
 
 	return is;
 }
@@ -97,6 +97,68 @@ ostream& operator<<(ostream& os, const Information& info)
 	cout << "Name: " << info._name << endl;
 	cout << "Sex: " << info._sex << endl;
 	cout << "Birthday: " << info._birthday << endl;
+	cout << "Username: " << info._username << endl;
 
 	return os;
+}
+
+bool Information::includeUsername(const User& user) {
+	return (this->_username == user.getUsername());
+}
+
+//Change and check success or not 
+bool Information::changeName(const string& name)
+{
+	const string regEx = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM ";
+	for (int i = 0; i < name.length(); i++)
+		if (regEx.rfind(name[i]) != string::npos)
+			return false;
+	
+	vector<string> tmpStr = Tokenizer::split(name, " ");
+	for (int i = 0; i < tmpStr.size(); i++)
+		tmpStr[i][0] = tolower(tmpStr[i][0]);
+
+	this->_name = Tokenizer::join(tmpStr, " ");
+	return true;
+}
+
+void Information::changeSex(const string& sex)
+{
+	this->_sex = sex;
+}
+
+bool Information::changeBirthday(const string& birthday)
+{
+	vector<string> date = Tokenizer::split(birthday, "/");
+	if (date.size() != 3)
+		return false;
+
+	for (int i = 0; i < date.size(); i++)
+		if (stoi(date[i]) <= 0)
+			return false;
+
+		switch (stoi(date[1]))
+	{
+	case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+		if (stoi(date[0]) > 31)
+			return false;
+		else
+			break;
+	case 4: case 6: case 9: case 11:
+		if (stoi(date[0]) > 30)
+			return false;
+		else
+			break;
+	case 2:
+		if (stoi(date[2]) % 400 != 0 && !(stoi(date[2]) % 4 == 0 && stoi(date[2]) % 100 != 0)) {
+			if (stoi(date[0]) > 28)
+				return false;
+		}
+		else 
+			if (stoi(date[0]) > 29)
+				return false;
+	}
+
+	this->_birthday = birthday;
+	return true;
 }
--- OOP-19CLC2-Group7/Bus-Reservation-System-C++-Project/Information.h
+++ OOP-19CLC2-Group7/Bus-Reservation-System-C++-Project/Information.h
@@ -2,6 +2,7 @@
 #define _INFORMATION_H_
 
 #include "Header.h"
+#include "User.h"
 
 class Information
 {
@@ -9,25 +10,29 @@ protected:
     string _name;
     string _sex; 
     string _birthday;
+    string _username;
 
 public:
     //Default constructor
     Information();
+    void updateUsername(const string& username);
 
-    //Copy constructor
+    //Copy constructors
     Information(const Information& info);
     
     //Operator
-    Information& operator=(const Information& info);
+    Information& operator =(const Information& info);
+
     friend ifstream& operator >>(ifstream& ifs, Information& info);
     friend ofstream& operator <<(ofstream& ofs, const Information& info);
     friend istream& operator >>(istream& is, Information& info);
     friend ostream& operator <<(ostream& os, const Information& info);
 
+    bool includeUsername(const User& user);
+
     bool changeName(const string& name);
     void changeSex(const string& sex);
     bool changeBirthday(const string& birthday);
-
 };
 
 #endif
