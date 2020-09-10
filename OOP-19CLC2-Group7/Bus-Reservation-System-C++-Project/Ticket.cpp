8f0d1fc5a98ffbc9671245bc092781983279f409
huyleminh <leminhhuy.hcmus@gmail.com>
2020-09-11 00:05:10 +0700

    Store ticket

---
+++ OOP-19CLC2-Group7/Bus-Reservation-System-C++-Project/Ticket.cpp
@@ -0,0 +1,52 @@
+#include "Ticket.h"
+#include "StoreTicket.h"
+
+//Plays Ticket 
+PlaysTicket::PlaysTicket(const string& type, const string& busID, const string& busName, const string& username) {
+	if (type == "Student")
+		this->_price = STUDENT_PLAYS;
+	else
+		this->_price = NORMAL_PLAYS;
+
+	this->_busID = busID;
+	this->_busName = busName;
+	this->_username = username;
+	this->_lifeCycle = 1;
+}
+
+void PlaysTicket::render()
+{
+	cout << "##############################################################################" << endl;
+	cout << "#                                                                            #" << endl;
+	cout << setw(25) << left << "# *Bus Id " << ": " << setw(50) << left << this->_busID << "#" << endl;
+	cout << setw(25) << left << "# *Bus name " << ": " << setw(50) << left << this->_busName << "#" << endl;
+	cout << setw(25) << left << "# *Price " << ": " << setw(50) << left << this->_price << "#" << endl;
+	cout << setw(25) << left << "# *Expriry date " << ": " << setw(50) << left << this->_lifeCycle << "#" << endl;
+	cout << "#                                                                            #" << endl;
+	cout << "##############################################################################" << endl;
+}
+
+MonthlyTicket::MonthlyTicket(const string& type, const string& busID, const string& busName, const string& username)
+{
+	if (type == "Student")
+		this->_price = STUDENT_MONTHLY;
+	else
+		this->_price = NORMAL_MONTHLY;
+
+	this->_busID = busID;
+	this->_busName = busName;
+	this->_username = username;
+	this->_lifeCycle = 1;
+}
+
+void MonthlyTicket::render()
+{
+	cout << "##############################################################################" << endl;
+	cout << "#                                                                            #" << endl;
+	cout << setw(25) << left << "# *Bus Id " << ": " << setw(50) << left << this->_busID << "#" << endl;
+	cout << setw(25) << left << "# *Bus name " << ": " << setw(50) << left << this->_busName << "#" << endl;
+	cout << setw(25) << left << "# *Price " << ": " << setw(50) << left << this->_price << "#" << endl;
+	cout << setw(25) << left << "# *Expriry date " << ": " << setw(50) << left << this->_lifeCycle << "#" << endl;
+	cout << "#                                                                            #" << endl;
+	cout << "##############################################################################" << endl;
+}
---
+++ OOP-19CLC2-Group7/Bus-Reservation-System-C++-Project/Ticket.h
@@ -0,0 +1,37 @@
+#ifndef _TICKET_H_
+#define _TICKET_H_
+
+#include "Header.h"
+#define STUDENT_PLAYS 3000
+#define STUDENT_MONTHLY 50000
+#define NORMAL_PLAYS 5000
+#define NORMAL_MONTHLY 100000
+
+
+class Ticket
+{
+protected:
+	int _price;
+	string _busID;
+	string _busName;
+	string _purchaseDate;
+	string _lifeCycle;
+	string _username;
+public:
+	//Print ticket
+	virtual void render() = 0;
+};
+
+class PlaysTicket : public Ticket { 
+public:
+	PlaysTicket(const string& type, const string& busID, const string& busName, const string& username);
+	void render();
+};
+
+class MonthlyTicket : public Ticket {
+public:
+	MonthlyTicket(const string& type, const string& busID, const string& busName, const string& username);
+	void render();
+};
+
+#endif
