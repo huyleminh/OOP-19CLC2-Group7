532c961a7fd15af5d73a3636418f4c7626ef7413
Akrlrd3306 <63332697+Akrlrd3306@users.noreply.github.com>
2020-10-12 22:11:35 +0700

    Feedback
    Done functions in menu
    Function save and read feeback from text file(not dome)

--- OOP-19CLC2-Group7/Bus-Reservation-System-C++-Project/Feedback/Feedback.h
+++ OOP-19CLC2-Group7/Bus-Reservation-System-C++-Project/Feedback/Feedback.h
@@ -6,42 +6,55 @@
 #include "../Workflow/LoginWorkflow/LoginWorkflow.h"
 #include "../Workflow/DecentralizeWorkflow/DecentralizeWorkflow.h"
 
-class NodeFeedback
-{
-private:
-	int feedback_number;
-	string Name; //
-	string date;
-	string x;
 
-	NodeFeedback* next;
-
-public:
-
-	friend class Feedback;
-
-};
 
 class Feedback : public User
 {
 private:
-	NodeFeedback* headlist;
+	vector<Feedback*> list;
 
 public:
 
 	Feedback();
 
-	//Methods
+	//Methods 
+	void addFeedback(Feedback* f);
 	void Feedbackmenu(User& user);
-	void createFeedback();
-	void viewFeedback();
-	void updateFeedback();
-	void printFeedback();
+
+	virtual void createFeedback();
+	virtual void updateFeedback();
+	virtual void viewFeedback();
+	
+	virtual void printFeedback();
 	void deleteFeedback();
 	void exit();
 
+	/*void loadFeedbackFile(Feedback* f);
+	void saveFeedbackFile();
+
+	void loadFeedbacktoFile();
+	void saveFeedbacKtoFile();*/
+
 };
 
+class NodeFeedback : public Feedback
+{
+private:
+
+	string Name; 
+	string date;
+	string x;
+
+public:
+
+	void createFeedback();
+	void updateFeedback();
+	void viewFeedback();
+	void printFeedback();
+
+	/*void loadFeedbacktoFile(NodeFeedback f);
+	void saveFeedbacKtoFile(NodeFeedback f);*/
+};
 
 
 
--- OOP-19CLC2-Group7/Bus-Reservation-System-C++-Project/main.cpp
+++ OOP-19CLC2-Group7/Bus-Reservation-System-C++-Project/main.cpp
@@ -6,6 +6,16 @@ int main() {
 	/*Menu menu;
 	menu.renderMainMenu();*/
 
+	Feedback list;
+	list.addFeedback(new NodeFeedback);
+	list.addFeedback(new NodeFeedback);
+	list.viewFeedback();
+	list.updateFeedback();
+	list.viewFeedback();
+
+	system("pause");
+	
+
 	return 0;
 
 }
